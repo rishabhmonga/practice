4e0130c254d6140e1f124f0b032b93d3c06e04da
rishabhmonga <rishabh.tar.gz@gmail.com>
2016-10-21 19:38:38 -0400

    remove nth node from last updates

--- leet/LinkedListRemoveNthNodeFromEnd.py
+++ leet/LinkedListRemoveNthNodeFromEnd.py
@@ -10,22 +10,20 @@ def removeNthFromEnd(head, n):
     :type n: int
     :rtype: ListNode
     """
-    curr = head
-    curr_n = curr
-    for i in range(n):
-        curr_n = curr_n.next
-    while curr_n is not None and curr_n.next is not None:
-        curr = curr.next
-        curr_n = curr_n.next
-    curr = delete_node(curr)
-    return head
+    start = ListNode(0)
+    slow = start
+    fast = start
+    slow.next = head
 
+    for i in range(n+1):
+        fast = fast.next
 
-def delete_node(node):
-    if node.next is None:
-        return None
-    node.next = node.next.next
-    return node
+    while fast is not None:
+        fast = fast.next
+        slow = slow.next
+
+    slow.next = slow.next.next
+    return start.next
 
 
 def list_print(head):
@@ -38,9 +36,9 @@ def list_print(head):
 
 if __name__ == '__main__':
     head = ListNode(1)
-    # head.next = ListNode(2)
-    # head.next.next = ListNode(3)
-    # head.next.next.next = ListNode(4)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
     list_print(head)
     head = removeNthFromEnd(head, 1)
     list_print(head)
